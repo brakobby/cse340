32e25f5066acd569611d598962e6529950f1f60f
brakobby <nkobby454@gmail.com>
2025-08-02 05:29:46 +0000

    committed

--- server.js
+++ server.js
@@ -2,11 +2,10 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-
-
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -15,13 +14,11 @@ const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/");
-
-
 app.use(express.static("public"));
 
 /* ***********************
  * View engine and Templates
- *************************/
+ *  ************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -32,7 +29,7 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 //Inventory Routes
 app.use("/inv", inventoryRoute)
  
@@ -53,26 +50,29 @@ app.use(async (req, res) => {
 const port = process.env.PORT
 const host = process.env.HOST
 
-
-
-
 /* ***********************
  *Express Error Handler
  *Place after all other middleware
  * ********************** */
-app.use(async(err, req, res, next) => {
+app.use(async(err,req,res,next) => {
   let nav = await utilities.getNav();
   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if(err.status==404){
+    message = err.message;
+  }else{
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
-} )
+})
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
+
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
