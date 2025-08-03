669c760d039d1ff9facd6f4aa76c1b6475045f83
brakobby <nkobby454@gmail.com>
2025-08-03 18:40:57 +0000

    server-side validation implemented

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require("../utilities/account-validation")
 
 /* ***********************
  * Route to Login Page
@@ -12,7 +13,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
  * Route to Register Page
  *************************/
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+router.post('/register', regValidate.registrationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount));
 
 /* ***********************
  * Export the Router
